bf93c34fa95b8e59e04adcffa18de21f5d1d56b2
Paul Billings <paulbillings101@yahoo.co.uk>
2020-11-08 20:01:53 +0000

    started booklist component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
+// CSS
+import "./index.css";
+
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h1>Paul Billings</h1>;
-const Message = () => {
-  return <p>This is my message</p>;
+const Book = () => {
+  return (
+    <article className="book">
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
 };
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/41i87iHnnJL._SX389_BO1,204,203,200_.jpg"
+      alt="The boy, the mole, the fox and the horse"
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>The Boy, The Mole, The Fox and The Horse</h1>;
+};
+
+const Author = () => {
+  return <h4>Charlie Mackesy</h4>;
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
